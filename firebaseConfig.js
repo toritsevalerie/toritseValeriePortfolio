576554e930489ec25687102fc3d460acdd08cec2
ToritseTuedor <torisevalerie@gmail.com>
2023-05-02 22:20:24 -0400

    added js files and firebase files

---
+++ firebaseConfig.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYjIBnkufWbg7vy7I-q9moA2XHP_10Ltg",
+
+  authDomain: "toritsevalerieportfolio.firebaseapp.com",
+
+  projectId: "toritsevalerieportfolio",
+
+  storageBucket: "toritsevalerieportfolio.appspot.com",
+
+  messagingSenderId: "571099258107",
+
+  appId: "1:571099258107:web:f645d3a8a9d351ff4b245e",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export default app; 
---
+++ script.js
@@ -0,0 +1,9 @@
+import app from "./firebaseConfig.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+
